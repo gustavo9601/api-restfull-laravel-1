95eeb9a1768e1d2c5a858b423a0cc02273dbca42
Gustavo Adolfo Marquez Prieto <gustavo.marquez@cun.edu.co>
2020-11-09 23:26:29 -0500

    All migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+USE App\User;
 
 class CreateUsersTable extends Migration
 {
@@ -20,6 +21,9 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('verified')->default(User::USUARIO_NO_VERIFICADO);
+            $table->string('verification_token')->nullable();
+            $table->string('admin')->default(User::USUARIO_REGULAR);
             $table->timestamps();
         });
     }
--- database/migrations/2020_11_10_025128_create_products_table.php
+++ database/migrations/2020_11_10_025128_create_products_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+USE App\Product;
 
 class CreateProductsTable extends Migration
 {
@@ -15,7 +16,15 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('quantity')->unsigned();
+            $table->string('status')->default(Product::PRODUCTO_NO_DISPONIBLE);
+            $table->string('image');
+            $table->bigInteger('seller_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('seller_id')->references('id')->on('users');
         });
     }
 
