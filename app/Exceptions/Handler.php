a431c99696de8a73def1733e2c62811777ec3ec9
Gustavo Adolfo Marquez Prieto <gustavo.marquez@cun.edu.co>
2020-11-10 02:22:39 -0500

    Control de exepciones

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,8 +3,15 @@
 namespace App\Exceptions;
 
 use App\Traits\ApiResponser;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -55,11 +62,58 @@ class Handler extends ExceptionHandler
     public function render($request, Throwable $exception)
     {
         // Verificando si la exepcion es de tipo validacion ejecutara la sobrescritura del metodo convertValidationExceptionToResponse
-        if ($exception instanceof ValidationException){
-         return $this->convertValidationExceptionToResponse($exception, $request);
+        if ($exception instanceof ValidationException) {
+            return $this->convertValidationExceptionToResponse($exception, $request);
         }
 
-        return parent::render($request, $exception);
+        // Execpcion de modelo no encuntrado, la lanza el findOrFail
+        if ($exception instanceof ModelNotFoundException) {
+            $model = class_basename($exception->getModel());
+            return $this->errorResponse('El recurso solicitado no existe, en el modelo ' . $model, 404);
+        }
+
+        // Exepcion de autenticacion
+        if ($exception instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $exception);
+        }
+
+        // Exepcion de autorizacion
+        if ($exception instanceof AuthorizationException) {
+            return $this->errorResponse('No posee permisos para ejecutar esta accion', 403);
+        }
+
+        // Exepcion de pagina no encontrada
+        if ($exception instanceof NotFoundHttpException) {
+            return $this->errorResponse('No se encontro la URL especificada', 404);
+        }
+
+        // Expecion de verbo no habilitado o no permitido al endpoint
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse('No es permitido el metodo', 404);
+        }
+
+        // Cualquier otra excepcion de tipo Http -> generalizando
+        if ($exception instanceof HttpException) {
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        // Execpcion de motor de base de datos, para este caso al intentar eliminar un objeto con relaciones activas
+        // Obteniendo el codigo de error 1451
+        if ($exception instanceof QueryException) {
+            $codigoErroBd = $exception->errorInfo[1];
+            if ($codigoErroBd == 1451) {
+                return $this->errorResponse('No se puede eliminar el recurso solicitado, ya que esta relacionado con otros modelos.', 409);
+            }
+        }
+
+        // Si esta en debug o desarrollo que muestre todos los errores
+        if (env('APP_DEBUG')){
+            return parent::render($request, $exception);
+        }
+
+        // Cualquier otra exepcion inesperada
+        return $this->errorResponse('Error inesperado, Intente luego', 500);
+
     }
 
     /**
@@ -73,7 +127,12 @@ class Handler extends ExceptionHandler
     {
         $errors = $e->validator->errors()->getMessages();
         return $this->errorResponse($errors, 422);
+    }
 
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse('No Autenticado.', 401);
     }
 
 }
