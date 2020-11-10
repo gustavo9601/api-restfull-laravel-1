e6c71f3af4dac7703acefd634dc776b4a8a745cf
Gustavo Adolfo Marquez Prieto <gustavo.marquez@cun.edu.co>
2020-11-10 14:01:31 -0500

    Save transaction

--- app/Http/Controllers/Transaction/TransactionController.php
+++ app/Http/Controllers/Transaction/TransactionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Transaction;
 
 use App\Http\Controllers\ApiController;
-use Illuminate\Http\Request;
+use App\Transaction;
 
 class TransactionController extends ApiController
 {
@@ -13,8 +13,21 @@ class TransactionController extends ApiController
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function index()
     {
-        //
+        $transactions = Transaction::all();
+
+        return $this->showAll($transactions);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Transaction $transaction)
+    {
+        return $this->showOne($transaction);
     }
 }
